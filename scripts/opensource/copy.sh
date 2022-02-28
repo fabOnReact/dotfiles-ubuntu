64dee41f0548a519408cb12b8c5b68c58fb98db8
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2022-02-28 10:03:53 +0800

    services function

--- scripts/opensource/copy.sh
+++ scripts/opensource/copy.sh
@@ -4,12 +4,10 @@ cp /home/fabrizio/.dotfiles/scripts/opensource/srails /usr/bin/srails
 cp /home/fabrizio/.dotfiles/scripts/opensource/run_test /usr/bin/run_test
 cp /home/fabrizio/.dotfiles/scripts/opensource/rtest /usr/bin/rtest
 cp /home/fabrizio/.dotfiles/scripts/opensource/sportfolio /usr/bin/sportfolio
-cp /home/fabrizio/.dotfiles/scripts/opensource/srn /usr/bin/srn
 cp /home/fabrizio/.dotfiles/scripts/opensource/sex /usr/bin/sex
 cp /home/fabrizio/.dotfiles/scripts/opensource/scomm /usr/bin/scomm
 cp /home/fabrizio/.dotfiles/scripts/opensource/algo /usr/bin/algo
 chmod +x /usr/bin/rtest
-chmod +x /usr/bin/srn
 chmod +x /usr/bin/sex
 chmod +x /usr/bin/scomm
 chmod +x /usr/bin/algo
