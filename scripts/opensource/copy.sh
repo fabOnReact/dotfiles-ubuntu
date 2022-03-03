8c20745657b6ef687daf7061a5a4b9a0ab0ef4ae
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2022-03-03 13:26:07 +0800

    metro and srn scripts

--- scripts/opensource/copy.sh
+++ scripts/opensource/copy.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 cp /home/fabrizio/.dotfiles/scripts/opensource/srn /usr/bin/srn
 chmod +x /usr/bin/srn
+cp /home/fabrizio/.dotfiles/scripts/opensource/metro /usr/bin/metro
+chmod +x /usr/bin/metro
